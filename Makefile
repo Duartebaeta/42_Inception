d7415b68ca291cae473d8426f685b1fd57f7f0cf
dhomem-d <baetini.mello@gmail.com>
2024-03-20 10:39:57 -0400

    ready to go kinda

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
 up:
-	# mkdir -p /home/inception/data/mysql
-	# mkdir -p /home/inception/data/wordpress
-	docker compose -f srcs/docker-compose.yml up -d
+	#mkdir -p /home/inception/data/mysql
+	#mkdir -p /home/inception/data/wordpress
+	docker-compose -f srcs/docker-compose.yml up -d
 
 down:
-	docker compose -f srcs/docker-compose.yml down
+	docker-compose -f srcs/docker-compose.yml down
 
-PHONY: up down
+restart:
+	docker-compose -f srcs/docker-compose.yml restart
+
+clean:
+	sudo rm -rf data/wordpress/*
+	sudo rm -rf data/mysql/*
+PHONY: up down restart
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -9,11 +9,19 @@ if [ ! -e "/var/www/html/wp-config.php" ]; then
 	wp config create --dbname=${DB_NAME} --path=/var/www/html/ --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=${DB_HOST} --allow-root
 	wp core install --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL} --allow-root
 	wp user create "${WP_USER}" "${WP_USER_EMAIL}" --user_pass="${WP_USER_PASS}" --role=author --display_name="${DB_USER}" --allow-root
-	wp theme install bravada --activate --allow-root
 else
 	echo "installed"
 
 fi
+
+# Always activate the desired theme
+THEME="astra"  # Change this to your desired theme
+if ! wp theme is-installed $THEME --allow-root; then
+    wp theme install $THEME --activate --allow-root
+else
+    wp theme activate $THEME --allow-root
+fi
+
 echo "Starting WordPress fastCGI on port 9000."
 
 exec /usr/sbin/php-fpm7.4 -F -R
